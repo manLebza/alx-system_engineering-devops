25a6dc9d63d9063823d2c8db7ab7324d6ec33ac9
manLebza <manlebza.mokoena1@gmail.com>
2023-02-09 15:35:15 +0000

    api tasks in

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script uses JSON placeholder API  to get Employee information """
+import CSV
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    userId = sys.argv[1]
+    user = '{}users/{}'.format(url, userId)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
+
+    todos = '{}todos?userId={}'.format(url, userId)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userId, name, task.get('completed'),
+                      task.get('title')])
+        filename = '{}.csv'.format(userId)
+        with open(filename, mode='w') as employee_file:
+            employee_writer = csv.writer(employee_file, delimiter=',',
+                                         quotechar='"', quoting=csv.QUOTE_ALL)
+
+            for task in l_task:
+                employee_writer.writerow(task)
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script uses JSON Placeholder API to fetch information about a employee """
+import json
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = '{}users'.format(url)
+    res = requests.get(user)
+    json_o = res.json()
+    d_task = {}
+    for user in json_o:
+        name = user.get('username')
+        userId = user.get('id')
+        todos = '{}todos?userId={}'.format(url, userId)
+        res = requests.get(todos)
+        task = res.json()
+        l_task = []
+        for task in tasks:
+            dict_task = {"username": name,
+                         "task": task.get('title'),
+                         "completed": task.get('completed')}
+            l_task.append(dict_task)
+
+        d_task[str(userId)] = l_task
+        filename = 'todo_all_employees.json'
+        with open(filename, mode='w') as f:
+            json.dump(d_task, f)
