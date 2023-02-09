0d07359ef27b7ddf03e5420b883e3df83829040a
manLebza <manlebza.mokoena1@gmail.com>
2023-02-09 17:21:38 +0000

    hot fix 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,8 +7,8 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
+    users = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(users)
     json_o = res.json()
     print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
